0f9a5a1364df7965f3f9c289251bf6fb08bfa73d
Sam <spatterson380@gmail.com>
2021-12-19 09:11:11 +0000

    initialising as a package

---
+++ docs/report.md
@@ -0,0 +1,19 @@
+# CityPulse Smart City Data ETL
+
+Author: Sam Patterson
+
+Date: 19.12.2012
+
+## Introduction
+
+## Explore and assess the data
+
+## Define the data model
+
+## Run ETL to model the data
+
+## Discuss future scenarios
+
+## Bonus questions
+
+## Summary
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "citypulse-etl",
+    version = "0.0.1",
+    author = "Sam Patterson",
+    description = "A Python extract, transform, load (ETL) pipeline for the CityPulse Smart City dataset.",
+    license = "MIT",
+    keywords = "citypulse extract-transform-load etl",
+    url = "https://gitlab.com/s-a-m/citypulse-etl",
+    package_dir={'':'src'},
+    packages=['citypulse_etl'],
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'pandas',
+        'python-dotenv',
+        'requests',
+        'SQLAlchemy',
+    ],
+    entry_points = {
+        # 'console_scripts': ['citypulse-etl=TBA'],
+    }
+)
